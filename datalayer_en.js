f7b76dcfe8572167a02e866442f17876551740d0
Kalpitha <kalpitha@sparshik.com>
2021-01-08 11:06:52 +0530

    updated

---
+++ datalayer_en.js
@@ -0,0 +1,580 @@
+
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+    language: "en",
+    section: "staycurious",
+    subSection: "stay connected",
+});
+
+/**Header**/
+
+ function staycurious_logo() 
+ {
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Header Interaction", 
+    action1: "Icon Click",
+    label1: "staycurious"
+ });
+ }
+
+ function lang_select()
+ {
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Header Interaction", 
+    action1: "Select Language",
+    label1: "en|ar"
+ });
+ }
+
+
+/**menu**/
+function menu_1() 
+ {
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Header Interaction", 
+    action1: "HamburgerNav",
+    label1: "stay curious"
+ });
+}
+
+ function menu_2() 
+ {
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Header Interaction", 
+    action1: "HamburgerNav",
+    label1: "stay adventurous"
+ });
+}
+
+ function menu_3() 
+ {
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Header Interaction", 
+    action1: "HamburgerNav",
+    label1: "stay indulgent"
+ });
+}
+
+ function menu_4() 
+ {
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Header Interaction", 
+    action1: "HamburgerNav",
+    label1: "stay enlightened"
+ });
+}
+
+ function menu_5() 
+ {
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Header Interaction", 
+    action1: "HamburgerNav",
+    label1: "stay creative"
+ });
+}
+
+ function menu_6() 
+ {
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Header Interaction", 
+    action1: "HamburgerNav",
+    label1: "stay entertained"
+ });
+}
+
+ function menu_7() 
+ {
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Header Interaction", 
+    action1: "HamburgerNav",
+    label1: "stay connected"
+ });
+}
+
+
+/** explore **/
+
+function fire_explore_ad_link_1()
+{
+ dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Get online with Abu Dhabi's Business Event Industry Partners - PCMA Digital Experience Institute", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "PCMA Digital Experience Institute"
+ });  
+}
+
+function fire_explore_ad_link_2()
+{
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Get online with Abu Dhabi's Business Event Industry Partners - IAEE Events and Education", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "IAEE Events and Education"
+ });
+}
+
+
+function fire_explore_ad_link_3()
+{
+ dataLayer.push({
+   event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Get online with Abu Dhabi's Business Event Industry Partners - ICCA Virtual Seminars", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "ICCA Virtual Seminars"
+ });  
+}
+
+function fire_explore_ad_link_4()
+{
+ dataLayer.push({
+   event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Get online with Abu Dhabi's Business Event Industry Partners - UFI Industry Resources", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "UFI Industry Resources"
+ });  
+}
+
+
+function discover_1()
+{
+ dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Video Play",
+    label1: "Discover Abu Dhabi Best Exhibition Spaces - Abu Dhabi National Exhibition Centre", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://adnec.ae/virtualtour"
+ });  
+}
+
+function discover_2()
+{
+ dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Video Play",
+    label1: "Discover Abu Dhabi Best Exhibition Spaces - Al Ain Convention Centre", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://alaincc.ae/virtualtour/"
+ });  
+}
+
+
+/** Interactive tour **/
+function fire_see_ad_link_1()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Brush up your skills at the MPI Academy - Risk Management Planning", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Risk Management Planning"
+ });  
+}
+
+function fire_see_ad_link_2()
+{
+ dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Brush up your skills at the MPI Academy - Rethinking the Incentive Trip", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Rethinking the Incentive Trip"
+ });   
+}
+
+function fire_see_ad_link_3()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Brush up your skills at the MPI Academy - Focused Sales Activity = Positive Sales Performance", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Focused Sales Activity = Positive Sales Performance"
+ });  
+}
+
+function fire_see_ad_link_4()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Brush up your skills at the MPI Academy - Content Strategies for the Evolving Marketplace", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Content Strategies for the Evolving Marketplace"
+ });  
+}
+
+function fire_see_ad_link_5()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Brush up your skills at the MPI Academy - MPI & The Student Experience", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "MPI & The Student Experience"
+ });  
+}
+
+function fire_see_ad_link_6()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Brush up your skills at the MPI Academy - Sustainability Now!", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Sustainability Now!"
+ });   
+}
+
+
+
+/** see abudhabi**/
+
+function fire_ad_see_1_title()
+{
+   dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Check out Skift Events and build your network - Short-Term Rentals", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Short-Term Rentals"
+ });   
+}
+
+function fire_ad_see_2_title()
+{
+   dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Check out Skift Events and build your network - Online Travel", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Online Travel"
+ });   
+}
+
+function fire_ad_see_3_title()
+{
+   dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Check out Skift Events and build your network - Hotels", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Hotels"
+ });    
+}
+
+function fire_ad_see_4_title()
+{
+   dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Check out Skift Events and build your network - Travel Advisors", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Travel Advisors"
+ });   
+}
+
+function fire_ad_see_5_title()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Check out Skift Events and build your network - Luxury", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Luxury"
+ });   
+}
+
+function fire_ad_see_6_title()
+{
+   dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Carousel Click",
+    label1: "Check out Skift Events and build your network - Explore more from Skift", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "Explore more from Skift"
+ });   
+}
+
+
+
+
+/** footer  */
+
+function fmenu_1()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Link Click",
+    label1: "Stay Curious", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://visitabudhabi.ae/stay-curious/staycurious.aspx"
+ });
+}
+
+function fmenu_2()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Link Click",
+    label1: "Stay Adventurous", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://visitabudhabi.ae/stay-adventurous/explore.aspx"
+ });
+}
+
+function fmenu_3()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Link Click",
+    label1: "Stay Indulgent", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://visitabudhabi.ae/stay-indulgent/culinary.aspx"
+ });
+}
+
+function fmenu_4()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Link Click",
+    label1: "Stay Enlightened", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://visitabudhabi.ae/stay-enlightened/learn.aspx"
+ });
+}
+
+function fmenu_5()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Link Click",
+    label1: "Stay Creative", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://visitabudhabi.ae/stay-creative/culture.aspx"
+ });
+}
+
+function fmenu_6()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Link Click",
+    label1: "Stay Entertained", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://visitabudhabi.ae/stay-entertained/events.aspx"
+ });
+}
+
+function fmenu_7()
+{
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Link Click",
+    label1: "Stay Connected", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://visitabudhabi.ae/stay-connected/business.aspx"
+ });
+}
+
+
+/** footer**/
+
+
+ function dct_logo()
+ {
+  dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Icon Click",
+    label1: "dct logo"
+ });
+ }
+
+function fire_foot_apple_store()
+ {
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Icon Click",
+    label1: "Apple PlayStore", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://itunes.apple.com/app/visit-abu-dhabi/id721678554?mt=8"
+ });
+}
+
+
+
+function fire_foot_google_store()
+ {
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Icon Click",
+    label1: "Google PlayStore", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://play.google.com/store/apps/details?id=com.visitabudhabi.android"
+ });
+}
+
+function fire_foot_fb()
+ {
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Icon Click",
+    label1: "Facebook", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://www.facebook.com/visitabudhabi"
+ });
+}
+
+function fire_foot_twt()
+ {
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Icon Click",
+    label1: "Twitter", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://twitter.com/visitabudhabi"
+ });
+}
+
+function fire_foot_insta()
+ {
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Icon Click",
+    label1: "Instagram", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://www.instagram.com/visitabudhabi/"
+ });
+}
+
+
+function fire_foot_ytb()
+{
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Footer Interaction", 
+    action1: "Icon Click",
+    label1: "youtube", 
+    category2: "Outbound Link Click",
+    action2: "https://visitabudhabi.ae/stay-connected/business.aspx",
+    label2: "https://www.youtube.com/user/VisitAbuDhabi"
+ });
+}
+
+
+/** slider swipe **/
+function mpi_academy_swipe_slider()
+{
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Swipe Click",
+    label1: "swipe - stay connected - brush up your skills at the mpi Academy"
+ });
+}
+
+function skift_events_swipe_slider()
+{
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Swipe Click",
+    label1: "swipe - stay connected - check out skift events and build your network"
+ });
+}
+
+
+function ad_bussiness_event_swipe_slider()
+{
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Swipe Click",
+    label1: "swipe - stay connected - get online with abudhabi's bussiness industry partners"
+ });
+}
+
+function discover_event_swipe_slider()
+{
+dataLayer.push({
+    event: "gaEvent",
+    category1: "Page Body Interaction", 
+    action1: "Swipe Click",
+    label1: "swipe - stay connected - discover abu dhabi best exhibition spaces"
+ });
+}
